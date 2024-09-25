7e3b753dbe6d4acefddc57566b4033689fc6a4fa
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-09-25 17:14:49 +0530

    context for global state management

---
+++ src/Context/MedContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useEffect, useState } from "react";
+import { doctors } from "../assets/assets"; 
+
+const MedContext = createContext();
+
+const MedContextProvider = ({ children }) => {
+
+    // storing all the nessecary data
+    const priceCurrency = "₹";
+
+    // navbar and sidebar toggle state - home page
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    // show top 10 doctors on homepage state - home page
+    const [topDoctors, setTopDoctors] = useState([])
+
+    useEffect(() => {
+        setTopDoctors(doctors.slice(0,10))
+    },[doctors])
+
+    const allvalues = {
+        menuVisible,
+        setMenuVisible,
+        priceCurrency,
+        topDoctors,
+    };
+
+    return (
+        <MedContext.Provider value={allvalues}>
+            {children}
+        </MedContext.Provider>
+    );
+};
+
+export { MedContext, MedContextProvider };
