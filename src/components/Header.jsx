749658f4061502451e63806177354e3c6e51664f
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-09-28 16:39:33 +0530

    modified the UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const navigate = useNavigate() 
 
   return (
-    <div className="flex flex-col md:flex-row bg-[#192de2] rounded-lg px-6 md:px-10 lg:px-20 py-8 mt-2">
+    <div className="flex flex-col md:flex-row bg-[#f9755b] rounded-lg px-6 md:px-10 lg:px-20 py-8 mt-2">
 
       {/* Left Side */}
       <div className="md:w-1/2 flex flex-col items-start justify-center gap-4">
@@ -16,7 +16,7 @@ const Header = () => {
           <p className="text-white sm:text-xl text-xs font-semibold sm:font-mono tracking-wider">
             Providing Expert Care When You Need It Most. <br className="hidden sm:block" /> Your Health is Our Priority
           </p>
-          <button onClick={() => navigate('/doctors')} className="sm:mt-4 mt-2 mb-4 inline-flex items-center bg-white text-[#4051ec] font-semibold sm:py-2 sm:px-4 
+          <button onClick={() => navigate('/doctors')} className="sm:mt-4 mt-2 mb-4 inline-flex items-center bg-white text-[#f9755b] font-semibold sm:py-2 sm:px-4 
           py-2 px-3 sm:text-sm text-xs rounded-lg hover:scale-110 transition ease-in duration-300 gap-2">
             Book Your Appointment <img src={assets.arrow_icon} alt="arrow icon" className="ml-2" />
           </button>
